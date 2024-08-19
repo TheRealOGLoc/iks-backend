cf453bb73356341f156978e56a049cb75fe12597
Yuchen Liu <lyc990312@hotmail.com>
2024-08-19 17:55:47 +1000

    Connect to the AWS s3 to store the images

--- types/generated/components.d.ts
+++ types/generated/components.d.ts
@@ -1,19 +1,5 @@
 import type { Schema, Attribute } from '@strapi/strapi';
 
-export interface LinksLink extends Schema.Component {
-  collectionName: 'components_links_links';
-  info: {
-    displayName: 'Link';
-  };
-  attributes: {
-    link: Attribute.String;
-  };
-}
-
 declare module '@strapi/types' {
-  export module Shared {
-    export interface Components {
-      'links.link': LinksLink;
-    }
-  }
+  export module Shared {}
 }
