bd864431cf71191ee3c62cb630b30f0d44770837
Yuchen Liu <lyc990312@hotmail.com>
2024-08-22 20:57:37 +1000

    Create the home page hero

--- types/generated/components.d.ts
+++ types/generated/components.d.ts
@@ -1,5 +1,24 @@
 import type { Schema, Attribute } from '@strapi/strapi';
 
+export interface HomepageElementsHero extends Schema.Component {
+  collectionName: 'components_homepage_elements_heroes';
+  info: {
+    displayName: 'Hero';
+  };
+  attributes: {
+    title: Attribute.String;
+    subtitle: Attribute.String;
+    description: Attribute.Text;
+    buttonText: Attribute.String;
+    buttonUrl: Attribute.String;
+    heroBackground: Attribute.Media<'images'>;
+  };
+}
+
 declare module '@strapi/types' {
-  export module Shared {}
+  export module Shared {
+    export interface Components {
+      'homepage-elements.hero': HomepageElementsHero;
+    }
+  }
 }
